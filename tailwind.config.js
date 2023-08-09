b4334cc581b108f7c46ea9fae10eb973b03ed5f7
Vyshnavi007 <vyshnavikrishna07@gmail.com>
2023-08-09 09:52:50 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {fontFamily:{mullish:["Mulish","sans-serif"],
+},
+      colors:{
+        deepblue: "#04032d",
+        lightblue:"hsla(212, 82.0%, 54.3%, 1)",
+        underline: "#61cea6"
+      },
+    },
+  },
+  plugins: [],
+}
+
